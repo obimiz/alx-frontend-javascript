a26ba9eeefe44f35ae1aba483088c76fdc871367
obimiz <mbanuobinna.com>
2024-08-01 17:05:20 +0100

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getBudgetObject(income, gdp, capita) {
+	const budget = {income, gdp, capita};
+
+	  return budget;
+}
