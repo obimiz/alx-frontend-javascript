be95f07fe1eac507563168977e8897b45c336bbe
obimiz <mbanuobinna.com>
2024-08-03 05:27:47 +0100

    for...of operator

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+	const newArray = [];
+	for (const value of array) {
+		newArray.push(appendstring + value)
+	}
+
+	return newArray;
+}
