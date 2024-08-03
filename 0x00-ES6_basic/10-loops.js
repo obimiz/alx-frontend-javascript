008cca6612a3a24bd36f070fc53b99fb3b490b2c
obimiz <mbanuobinna.com>
2024-08-03 05:32:20 +0100

    for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -3,7 +3,7 @@
 export default function appendToEachArrayValue(array, appendString) {
 	const newArray = [];
 	for (const value of array) {
-		newArray.push(appendstring + value);
+		newArray.push(appendString + value);
 	}
 
 	return newArray;
