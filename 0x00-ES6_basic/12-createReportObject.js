81b88c09970936b5b05f936cab2be589e0140f76
obimiz <mbanuobinna.com>
2024-08-03 06:57:23 +0100

    create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -4,7 +4,7 @@ export default function createReportObject(employeesList) {
 	return{
 		allEmployees: employeesList,
 		getNumberOfDepartments(employeesList){
-			return object.keys(employeesList).Length;
+			return Object.keys(employeesList).length;
 		},
 	};
 }
