a50d043d77ca7280da64ea7fa05819518813891f
obimiz <mbanuobinna.com>
2024-08-07 11:41:44 +0100

    return new promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,11 +2,11 @@
 /* eslint-disable */
 export default function function getResponseFromAPI() {
 	const success = true;
-	return new Promise ((resolve, reject) => {
-		if success{
-			resolve():
+	return new Promise((resolve, reject) => {
+		if (success) {
+			resolve();
 		}	else{
 				reject();
 		}
-	})
+	});
 }
